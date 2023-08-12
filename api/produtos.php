2652931e5705fc4935e3476d9ce218d80a59b2d1
Paulo Gustavo <pgustavo-siqueira@hotmail.com>
2023-08-12 16:27:29 -0300

    Projeto PHP

---
+++ api/produtos.php
@@ -0,0 +1,59 @@
+<?php
+
+    $produtos = array(
+        (object) array(
+            "nome" =>  "VENTOINHA COUGAR MHP 120, 120MM, BRANCO, CF-MHP12HB-W",
+            "preco" =>  119.99,
+            "imagem" => "../img/cooler.jpg"
+        ),
+        (object) array(
+            "nome" =>  "PLACA DE VIDEO MANCER RTX 2060 SUPER HEIMDALL SS, 8GB, GDDR6, 256-BIT, MCR-RTX2060SUPER-HMDLLSS",
+            "preco" =>  1799.99,
+            "imagem" => "../img/placa de video.jpg"
+        ),
+        (object) array(
+            "nome" =>  "MONITOR GAMER PICHAU CENTAURI CRS24B, 23.8 POL., IPS, FHD, 1MS, 165HZ, FREESYNC, HDMI/DP, PG-CTRS24B-BL01",
+            "preco" =>  889.99,
+            "imagem" => "../img/monitor.jpg"
+        ),
+        (object) array(
+            "nome" =>  "MONITOR PROFISSIONAL PICHAU PERSEUS P1, MINILED, 27 POL., IPS, QHD, 1MS, 165HZ, FREESYNC, HDMI/DP, PG-PRSPML-BL01",
+            "preco" =>  4999.99,
+            "imagem" => "../img/monitor profissional.jpg"
+        ),
+        (object) array(
+            "nome" =>  "GABINETE GAMER PICHAU HX550M, MID-TOWER, LATERAL DE VIDRO, COM 3 FANS, BRANCO, PG-HX55M-WT01",
+            "preco" =>  269.19,
+            "imagem" => "../img/gabinete.jpg"
+        ),
+        (object) array(
+            "nome" =>  "WATER COOLER PICHAU GAMING AQUA 120 PRO, ARGB, 120MM, PG-AQ120PRO-ARGB01",
+            "preco" =>  319.99,
+            "imagem" => "../img/water cooler.jpg"
+        ),
+        (object) array(
+            "nome" =>  "COMPUTADOR PICHAU GAMER BARBATOS II, INTEL I5-11400F, GEFORCE RTX 2060 SUPER 8GB, 16GB DDR4, SSD M.2 480GB",
+            "preco" =>  3999.99,
+            "imagem" => "../img/gabinete pulsar.jpg"
+        ),
+        (object) array(
+            "nome" =>  "GABINETE DT3 HYPER, LATERAL E FRONTAL DE VIDRO, PRETO, 13872-1",
+            "preco" =>  319.99,
+            "imagem" => "../img/gabinete dt3.jpg"
+        ),
+        (object) array(
+            "nome" =>  "MONITOR GAMER SAMSUNG ODYSSEY G30, 24 POL VA, FHD, 1MS, 144HZ, FREESYNC, HDMI/DP, LS24BG300ELMZD",
+            "preco" =>  1249.99,
+            "imagem" => "../img/monitor gamer.jpg"
+        ),
+        (object) array(
+            "nome" =>  "MONITOR GAMER REDRAGON QUARTZ, 23.8 POL., VA, FHD, 1MS, 165HZ, FREESYNC, HDMI/DP GM24H2G",
+            "preco" =>  1049.99,
+            "imagem" => "../img/monitor reddragon.jpg"
+        ),
+
+    )
+
+
+
+?>
